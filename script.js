e84f4981fc4355e8bf09bf8d9d4a1d9d149b2f5a
Gopi2923 <dgopidhikshan@gmail.com>
2024-04-12 22:34:57 +0530

    Format changes

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> Invalid input, 
 
 
 function showToast(msg) {
-   let toast = document.createElement('div');
-   toast.classList.add('toast');
-   toast.innerHTML = msg;
-   toastBox.appendChild(toast);
+    let toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.innerHTML = msg;
+    toastBox.appendChild(toast);
 
-   if(msg.includes('error')) {
-    toast.classList.add('error')
-   }
-   if(msg.includes('Invalid')) {
-    toast.classList.add('invalid')
-   }
+    if (msg.includes('error')) {
+        toast.classList.add('error')
+    }
+    if (msg.includes('Invalid')) {
+        toast.classList.add('invalid')
+    }
 
-   setTimeout(() => {
-    toast.remove()
-   }, 6000)
+    setTimeout(() => {
+        toast.remove()
+    }, 6000)
 }
